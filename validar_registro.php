435ecb418946ede949ee1174501baad385a5528d
EVM <elyzeo.vm@gmail.com>
2019-11-26 16:39:03 -0600

    Creando una funcion para el registro de los eventos

--- validar_registro.php
+++ validar_registro.php
@@ -20,14 +20,19 @@
         include_once 'includes/funciones/funciones.php';
         $pedido=productos_json($boletos, $camisas, $etiquetas);
         
-        echo "<pre>";
-            var_dump($pedido);
-        echo "</pre>";
+        // Eventos
+        $eventos=$_POST['registro'];
+        $registro=eventos_json($eventos);
 
     ?>
+    <pre>
+        <?php var_dump($pedido); ?>
+    </pre>
+
+    <hr>
 
     <pre>
-        <?php var_dump($boletos); ?>
+        <?php var_dump($registro); ?>
     </pre>
     <?php endif; ?>
 
