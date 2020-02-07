1211184218c607e11165458cd2c8c2fb0d51ea30
EliseoVM <elyzeo.vm@gmail.com>
2020-02-07 12:40:14 -0600

    Ajustando el HTML de nuestro formulario

--- pagar.php
+++ pagar.php
@@ -15,17 +15,53 @@ use PayPal\Api\Payment;
 
 require 'includes/paypal.php';
 
-/*
-$produto = htmlspecialchars($_POST['producto']);
-$precio = htmlspecialchars($_POST['precio']);
-$precio = (int) $precio;
-var_dump($precio);
-$envio = 0;
-$total = $precio + $envio;
+if(isset($_POST['submit'])):
+        $nombre=$_POST['nombre'];
+        $apellido=$_POST['apellido'];
+        $email=$_POST['email'];
+        $regalo=$_POST['regalo'];
+        $total=$_POST['total_pedido'];
+        $fecha=date('Y-m-d H:i:s');
+        // Pedidos
+        $boletos=$_POST['boletos'];
+        $camisas=$_POST['pedido_extra']['camisas']['cantidad'];
+        $precioCamisa=$_POST['pedido_extra']['camisas']['precio'];
+
+        $etiquetas=$_POST['pedido_extra']['etiquetas']['cantidad'];
+        $precioEtiquetas=$_POST['pedido_extra']['etiquetas']['precio'];
+
+        // Llamamos a la funcion JSON
+        include_once 'includes/funciones/funciones.php';
+        $pedido=productos_json($boletos, $camisas, $etiquetas);
+        // Eventos
+        $eventos=$_POST['registro'];
+        $registro=eventos_json($eventos);
+
+        try {
+                require_once('includes/funciones/bd_conexion.php');
+                // Preparamos los datos prepare
+                $stmt=$conn->prepare("INSERT INTO registrados (nombre_registrado, apellido_registrado, email_registrado, fecha_registro, pases_articulos, talleres_registrados, regalo, total_pagado) VALUES (?,?,?,?,?,?,?,?) ");
+                // Decimos que datos vamos a dar bind_param
+                // Las s represetan cadenas, la i representa un entero
+                $stmt->bind_param("ssssssis", $nombre, $apellido, $email, $fecha, $pedido, $registro, $regalo, $total);
+                // Executamos los datos execute
+                $stmt->execute();
+                // Cerramos el stamend
+                $stmt->close();
+                // Cerramos la conexion
+                $conn->close();
+                // Evita meter registros repetidos al cargar de nuevo una pagina
+                header('Location: validar_registro.php?exitoso=1');
+        } catch (\Exception $e) {
+                $error = $e->getMessage();
+        }
+
+
 
 $compra = new Payer();
 $compra->setPaymentMethod('paypal');
 
+/*
 $articulo = new Item();
 $articulo->setName($produto)
         ->setCurrency('MXN')
@@ -76,5 +112,4 @@ $aprobado = $pago->getApprovalLink();
 
 header("Location: {$aprobado}");
 */
-
 ?>
