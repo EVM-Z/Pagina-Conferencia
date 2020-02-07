ab37a038d64b553199f0079f80b59715d91944aa
EliseoVM <elyzeo.vm@gmail.com>
2020-02-06 23:35:39 -0600

    Generando las cuentas y la App en PayPal

--- includes/paypal.php
+++ includes/paypal.php
@@ -7,8 +7,8 @@ define ('URL_SITIO', 'http://localhost/yo-gdlwebcam');
 // Instalamos el SDK
 $apiContext = new \PayPal\Rest\ApiContext(
     new \PayPal\Auth\OAuthTokenCredential(
-        'AfS0V-pSQxvHX1TV27xeOqTQFkU-lBcxMCWZnWvs_LPOHnTSeycYw_LiVcYj0trVhYyjM7LJXVOXM07c', // ClienteID
-        'EJpFjh2v4Ku1sV-JL-kjXHDMHRUBBYSW4M47Uf1Wri6iboonHaxIyF7HtrugwHfdv6lbt3P2FxFPJiRm' // Secret        
+        'ATfwYIhqaZT33fqTANTm1PdMl2Fb0XjRuKKiYv7oNZXudcHOemUwIYGWlWy6yKm1fYsXQfKOR30vkmjM', // ClienteID
+        'EG6HRM_88YdQ2RkvxxSZ-9YsF-ENCiUsd14BP3pwC0an8Vf5dxX2iEsHbcDCW0GfDq1UY7XukP47JtE_' // Secret        
     )
 );
 
