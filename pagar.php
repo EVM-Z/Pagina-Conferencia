d9b6de751fff126319ad215a3dcbb9cdd62d1005
EliseoVM <elyzeo.vm@gmail.com>
2020-02-15 12:09:03 -0600

    Verificando los passwords via PayPal REST API

--- pagar.php
+++ pagar.php
@@ -133,8 +133,8 @@ $transaccion->setAmount($cantidad)
         
 
 $redireccionar = new RedirectUrls();
-$redireccionar->setReturnUrl(URL_SITIO . "/pago_finalizado.php?exito=true&id_pago={$ID_registro}")
-        ->setCancelUrl(URL_SITIO . "/pago_finalizado.php?exito=false$id_pago={$ID_registro}");
+$redireccionar->setReturnUrl(URL_SITIO . "/pago_finalizado.php&id_pago={$ID_registro}")
+                ->setCancelUrl(URL_SITIO . "/pago_finalizado.php&id_pago={$ID_registro}");
 
 
 
--- pago_finalizado.php
+++ pago_finalizado.php
@@ -1,13 +1,33 @@
+<?php include_once 'includes/templates/header.php';
 
-<?php include_once 'includes/templates/header.php'; ?>
+// Contiene las credenciales del cliente y secret
+use PayPal\Rest\ApiContext;
+use PayPal\Api\PaymentExecution;
+use PayPal\Api\Payment;
+require 'includes/paypal.php';
+
+?>
 <section class="seccion contenedor">
     <h2>Resumen Registro</h2>
     <?php
-        $resultado = $_GET['exito'];
         $paymentId = $_GET['paymentId'];
         $id_pago = (int) $_GET['id_pago'];
 
-        if ($resultado == "true") {
+        // Paticion a REST API
+        $pago = Payment::get($paymentId, $apiContext);
+        $execution = new PaymentExecution();
+        // Especificamos el pago
+        $execution->setPayerId($_GET['PayerID']);
+        
+        // Resultado tiene la informacion de la transaccion
+        $resultado = $pago->execute($execute, $apiContext);
+
+        $respuesta = $resultado->transactions[0]->related_resources[0]->sale->state;
+
+        // var_dump($respuesta);
+
+
+        if ($resultado == "completed") {
             echo "<div class'resultado correcto'>";
             echo "El pago se realiazo correctamente <br>";
             echo "El ID es {$paymentId}";
