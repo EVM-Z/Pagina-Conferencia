fc0d24d3c2d4abe03d04ed80adcb61853034f04b
EliseoVM <elyzeo.vm@gmail.com>
2020-02-26 04:12:09 -0600

    Ajustes al diseño de Admin LTE

--- admin/pages/examples/admin-area.php
+++ admin/pages/examples/admin-area.php
@@ -1,7 +1,7 @@
 <?php
 include 'templates/header.php';
-include 'templates/barra.php';
-include 'templates/navegacion.php';
+include 'templates/barra-superior.php';
+include 'templates/navegacion-lateral.php';
 ?>
 
 
@@ -34,13 +34,6 @@ include 'templates/navegacion.php';
                 <div class="card">
                     <div class="card-header">
                         <h3 class="card-title">Title</h3>
-
-                        <div class="card-tools">
-                            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-              <i class="fas fa-minus"></i></button>
-                            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fas fa-times"></i></button>
-                        </div>
                     </div>
                     <div class="card-body">
                         Start creating your amazing application!
