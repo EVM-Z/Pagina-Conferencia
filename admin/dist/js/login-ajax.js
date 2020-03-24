9eaabec5e5e5744c4d60c1d3e1f73aa907cd59fa
EliseoVM <elyzeo.vm@gmail.com>
2020-03-23 19:39:45 -0600

    Moviendo los registros de login

---
+++ admin/dist/js/login-ajax.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $('#login-admin').on('submit', function(e) {
+        // Evitamos que se habra el archivo insertar-admin.php desde el formulario crear-admin.php
+        e.preventDefault();
+        // Obtener los datos
+        var datos = $(this).serializeArray();
+        $.ajax({
+            type: $(this).attr('method'),
+            data: datos,
+            url: $(this).attr('action'),
+            dataType: 'json',
+            success: function(data) {
+                console.log(data);
+                var resultado = data;
+                if (resultado.respuesta == 'exitoso') {
+                    Swal.fire(
+                        'Login Correcto',
+                        'Bievenid@ ' + resultado.usuario,
+                        'success'
+                    )
+                    setTimeout(function() {
+                        window.location.href = 'admin-area.php';
+                    }, 2000);
+                } else {
+                    Swal.fire(
+                        'Error',
+                        'Usuario o Password Incorrecto',
+                        'error'
+                    )
+                }
+            }
+        });
+    });
+});
