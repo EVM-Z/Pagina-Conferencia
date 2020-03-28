48f375f9d4c79ea9207d356fcf222d72519ea47f
EliseoVM <elyzeo.vm@gmail.com>
2020-03-27 23:43:35 -0600

    Mostrando todos los eventos en DataTables

--- admin/pages/examples/lista-admin.php
+++ admin/pages/examples/lista-admin.php
@@ -7,92 +7,90 @@ include 'templates/barra-superior.php';
 include 'templates/navegacion-lateral.php';
 ?>
 
-
-        
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Listado de Administradores</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">DataTables</li>
-            </ol>
-          </div>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Listado de Administradores</h1>
         </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="row">
-        <div class="col-12">
-          <div class="card">
-            <div class="card-header">
-              <h3 class="card-title">Maneja los usuarios en esta sección</h3>
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-              <table id="registros" class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                  <th>Usuarios</th>
-                  <th>Nombre</th>
-                  <th>Acciones</th>
-                </tr>
-                </thead>
-                <tbody>
-
-                  <?php
-                  try {
-                    $sql = "SELECT id_admin, usuario, nombre FROM admins";
-                    $resultado = $conn->query($sql);
-                  } catch (Exception $e) {
-                    $error = $e->getMessage();
-                    echo $error;
-                  }
-                  while($admin = $resultado->fetch_assoc()) { ?>
-                    <tr>
-                      <td><?php echo $admin['usuario']; ?></td>
-                      <td><?php echo $admin['nombre']; ?></td>
-                      <td>
-                        <a href="editar-admin.php?id=<?php echo $admin['id_admin'] ?>" class="btn bg-gradient-warning btn-sm margin">
-                        <i class="fas fa-pencil-alt"></i>
-                        </a>
-                        <a href="#" data-id="<?php echo $admin['id_admin']; ?>" data-tipo="admin" class="btn bg-gradient-danger btn-sm borrar_registro">
-                        <i class="fas fa-trash-alt"></i>
-                        </a>
-                      </td>
-                    </tr>
-
-                  <?php } ?>
-
-                </tbody>
-                <tfoot>
-                <tr>
-                  <th>Usuarios</th>
-                  <th>Nombre</th>
-                  <th>Acciones</th>
-                </tr>
-                </tfoot>
-              </table>
-            </div>
-            <!-- /.card-body -->
-          </div>
-          <!-- /.card -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">DataTables</li>
+          </ol>
         </div>
-        <!-- /.col -->
       </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Maneja los usuarios en esta sección</h3>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <table id="registros" class="table table-bordered table-striped">
+              <thead>
+              <tr>
+                <th>Usuarios</th>
+                <th>Nombre</th>
+                <th>Acciones</th>
+              </tr>
+              </thead>
+              <tbody>
+
+                <?php
+                try {
+                  $sql = "SELECT id_admin, usuario, nombre FROM admins";
+                  $resultado = $conn->query($sql);
+                } catch (Exception $e) {
+                  $error = $e->getMessage();
+                  echo $error;
+                }
+                while($admin = $resultado->fetch_assoc()) { ?>
+                  <tr>
+                    <td><?php echo $admin['usuario']; ?></td>
+                    <td><?php echo $admin['nombre']; ?></td>
+                    <td>
+                      <a href="editar-admin.php?id=<?php echo $admin['id_admin'] ?>" class="btn bg-gradient-warning btn-sm margin">
+                      <i class="fas fa-pencil-alt"></i>
+                      </a>
+                      <a href="#" data-id="<?php echo $admin['id_admin']; ?>" data-tipo="admin" class="btn bg-gradient-danger btn-sm borrar_registro">
+                      <i class="fas fa-trash-alt"></i>
+                      </a>
+                    </td>
+                  </tr>
+
+                <?php } ?>
+
+              </tbody>
+              <tfoot>
+              <tr>
+                <th>Usuarios</th>
+                <th>Nombre</th>
+                <th>Acciones</th>
+              </tr>
+              </tfoot>
+            </table>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
 
 
 <?php
--- admin/pages/examples/templates/navegacion-lateral.php
+++ admin/pages/examples/templates/navegacion-lateral.php
@@ -52,13 +52,13 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link">
+                                    <a href="lista-evento.php" class="nav-link">
                                         <i class="fas fa-list-ul"></i>
                                         <p>Ver Todos</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link">
+                                    <a href="editar-evento.php" class="nav-link">
                                         <i class="fas fa-plus-circle"></i>
                                         <p>Agregar</p>
                                     </a>
