e865094068d1e543f6c2e9b82d492fb005d0e601
EliseoVM <elyzeo.vm@gmail.com>
2020-10-07 23:25:09 -0600

    Creando widgets con los regalos

--- admin/pages/examples/dashboard.php
+++ admin/pages/examples/dashboard.php
@@ -123,8 +123,75 @@ include 'templates/navegacion-lateral.php';
                     </div>
                 </div>
                 <!-- ./col -->
+            </div>
 
+            <h2 class="page-header">Regalos</h2>
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <?php
+                        $sql = "SELECT COUNT(total_pagado) AS pulseras FROM registrados WHERE regalo = 1 AND pagado = 1 ";
+                        $resultado = $conn->query($sql);
+                        $regalo = $resultado->fetch_assoc();
+                    ?>
+                    <!-- small card -->
+                    <div class="small-box bg-pink">
+                    <div class="inner">
+                        <h3><?php echo (float) $regalo['pulseras']; ?></h3>
+                        <p>Pulseras</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-gift"></i>
+                    </div>
+                    <a href="lista-registrados.php" class="small-box-footer">
+                        Más información <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                    </div>
+                </div>
+                <!-- ./col -->
 
+                <div class="col-lg-3 col-6">
+                    <?php
+                        $sql = "SELECT COUNT(total_pagado) AS etiquetas FROM registrados WHERE regalo = 2 AND pagado = 1 ";
+                        $resultado = $conn->query($sql);
+                        $regalo = $resultado->fetch_assoc();
+                    ?>
+                    <!-- small card -->
+                    <div class="small-box bg-yellow">
+                    <div class="inner">
+                        <h3><?php echo (float) $regalo['etiquetas']; ?></h3>
+                        <p>Etiquetas</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-clipboard-list"></i>
+                    </div>
+                    <a href="lista-registrados.php" class="small-box-footer">
+                        Más información <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                    </div>
+                </div>
+                <!-- ./col -->
+
+                <div class="col-lg-3 col-6">
+                    <?php
+                        $sql = "SELECT COUNT(total_pagado) AS plumas FROM registrados WHERE regalo = 3 AND pagado = 1 ";
+                        $resultado = $conn->query($sql);
+                        $regalo = $resultado->fetch_assoc();
+                    ?>
+                    <!-- small card -->
+                    <div class="small-box bg-orange">
+                    <div class="inner">
+                        <h3><?php echo (float) $regalo['plumas']; ?></h3>
+                        <p>Plumas</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-pencil-alt"></i>
+                    </div>
+                    <a href="lista-registrados.php" class="small-box-footer">
+                        Más información <i class="fas fa-arrow-circle-right"></i>
+                    </a>
+                    </div>
+                </div>
+                <!-- ./col -->
             </div>
                 
 
